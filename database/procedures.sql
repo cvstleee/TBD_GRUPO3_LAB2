656324b776b65d0f1f807056adac1038a27c5d1d
Agustin Vera <agustin.vera@usach.cl>
2024-12-09 18:51:50 -0300

    fix(database): Resolved parameter name conflict for 'category_id' in the stored procedure to update product prices

--- database/procedures.sql
+++ database/procedures.sql
@@ -31,13 +31,13 @@ END;
 $$;
 
 -- Actualizar precios de los productos de una categoria en base a un porcentaje de incremento
-CREATE OR REPLACE PROCEDURE update_products_prices_with_rate(rate NUMERIC, category_id INT)
+CREATE OR REPLACE PROCEDURE update_products_prices_with_rate(rate NUMERIC, category_id_param INT)
 LANGUAGE plpgsql
 AS $$
 BEGIN
     UPDATE products
     SET price = price * (1 + (rate / 100))
-    WHERE products.category_id = category_id;
+    WHERE products.category_id = category_id_param;
 END;
 $$;
 
@@ -71,4 +71,4 @@ $$
  LANGUAGE plpgsql;
 
 -- Uso del procedimiento
--- SELECT * FROM get_user_operations_report();
+-- SELECT * FROM get_client_operations_report();
